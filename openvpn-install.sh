2de99e5a1aafbbed7798cb129695d78fb9d5edd2
monkcl <daniel.gekhman@gmail.com>
2023-10-20 17:18:28 +0300

    add variables dns, protocol

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -167,6 +167,9 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 	echo "   1) UDP (recommended)"
 	echo "   2) TCP"
 	read -p "Protocol [1]: " protocol
+	
+	protocol=2
+
 	until [[ -z "$protocol" || "$protocol" =~ ^[12]$ ]]; do
 		echo "$protocol: invalid selection."
 		read -p "Protocol [1]: " protocol
@@ -196,6 +199,9 @@ if [[ ! -e /etc/openvpn/server/server.conf ]]; then
 	echo "   5) Quad9"
 	echo "   6) AdGuard"
 	read -p "DNS server [1]: " dns
+
+	dns=2
+	
 	until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
 		echo "$dns: invalid selection."
 		read -p "DNS server [1]: " dns
